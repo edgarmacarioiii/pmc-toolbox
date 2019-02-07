e2f921fcd80d0a5eace7bf7a745e786267e73491
Edgar Q. Macario III <emacario@emacario.local>
2019-02-07 15:56:13 +0800

    Add Kg to Lb converter

--- library/src/main/java/com/paulmarkcastillo/pmctoolbox/converters/WeightConverter.kt
+++ library/src/main/java/com/paulmarkcastillo/pmctoolbox/converters/WeightConverter.kt
@@ -1,9 +1,14 @@
 package com.paulmarkcastillo.pmctoolbox.converters
 
+import kotlin.math.roundToInt
+
 class WeightConverter : BaseConverter() {
 
     fun convertLbToKg(lb: Double): String {
         return decimalFormat.format(lb * 0.45359237)
     }
 
+    fun convertKgToLb(kg: Double): String {
+        return decimalFormat.format((kg * 2.20462).roundToInt())
+    }
 }
