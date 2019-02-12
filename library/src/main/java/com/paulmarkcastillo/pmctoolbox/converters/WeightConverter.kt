23f0fca298c4961a12cb803dc7e36b48663b71a2
Edgar Q. Macario III <emacario@emacario.local>
2019-02-12 09:34:39 +0800

    Update KgToLb method to return result as Double

--- library/src/main/java/com/paulmarkcastillo/pmctoolbox/converters/WeightConverter.kt
+++ library/src/main/java/com/paulmarkcastillo/pmctoolbox/converters/WeightConverter.kt
@@ -1,14 +1,12 @@
 package com.paulmarkcastillo.pmctoolbox.converters
 
-import kotlin.math.roundToInt
-
 class WeightConverter : BaseConverter() {
 
     fun convertLbToKg(lb: Double): Double {
         return lb * 0.45359237
     }
 
-    fun convertKgToLb(kg: Double): String {
-        return decimalFormat.format((kg * 2.20462).roundToInt())
+    fun convertKgToLb(kg: Double): Double {
+        return kg * 2.20462
     }
 }
